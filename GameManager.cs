9f0240d9fbef1d5b17ce7b027aa54469d0df4c2f
Jack Hancock <77704393+JackalGame@users.noreply.github.com>
2021-02-21 17:41:02 +0000

    Scripts

---
+++ GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int score;
+    [SerializeField] Text scoreText;
+    [SerializeField] Text highScoreText;
+    public bool gameStarted;
+
+    private void Awake()
+    {
+        highScoreText.text = "Best: " + GetHighScore().ToString();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartGame();
+        }
+    }
+    
+    public void StartGame()
+    {
+        gameStarted = true;
+        FindObjectOfType<Road>().StartBuilding();
+    }
+    
+    public void EndGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+        
+        if(score > GetHighScore())
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highScoreText.text = "Best: " + score.ToString();
+        }
+    }
+
+    public int GetHighScore()
+    {
+        int i = PlayerPrefs.GetInt("HighScore");
+        return i;
+    }
+}
---
+++ RoadPart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadPart : MonoBehaviour
+{
+    [Tooltip("In Seconds")] [SerializeField] float waitToDestroy = 2f;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (FindObjectOfType<GameManager>().gameStarted)
+        {
+            StartCoroutine(DestroyRoadPart());
+        }
+    }
+
+    IEnumerator DestroyRoadPart()
+    {
+        yield return new WaitForSeconds(waitToDestroy);
+        Destroy(gameObject);
+    }
+}
